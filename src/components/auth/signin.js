75cf15df8a55cc4701c9f6fbe4ad51906f4bbb99
Song Ji <songji0509@gmail.com>
2017-09-28 14:16:06 -0700

    Wire up signin page

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
+import { reduxForm } from 'redux-form';
 
-class Header extends Component{
+class Signin extends Component{
+  handleFormSubmit({email, password}){
+    console.log(email, password);
+  }
+  
+  
   render(){
+    const {handleSubmit, fields: {email, password}} = this.props;
+    
     return(
-      <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign in
-          </li>
-        </ul>
-      </nav>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input {...email} className="form-control"/>
+        </fieldset>
+        
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input {...password} type="password" className="form-control"/>
+        </fieldset>
+        <button action='submit' className="btn btn-primary">Sign in</button>
+      </form>
     )
   }
 }
 
-export default Header;
+export default reduxForm({
+  form: "signin", // name of the form
+  fields: ['email', 'password']
+})(Signin);
