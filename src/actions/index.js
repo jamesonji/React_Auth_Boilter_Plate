2957750ccc2c02f0619a69f0e93cd8c2cd285168
Song Ji <songji0509@gmail.com>
2017-09-28 16:17:20 -0700

    Signin in progess

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
-const ROOT_URL = 'http://localhost:3000';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
+const ROOT_URL = 'http://localhost:3000'; // server location
 
 export function signinUser({email, password}){
   return function(dispatch){
     //Submit email/password to server
     axios.post(`${ROOT_URL}/signin`, {email, password})
+      .then(response => {
+        console.log(response);
+        // if request is good..
+        // - update state to indicated user is authenticated
+        dispatch({type: AUTH_USER})
+        
+        // - save the JWT token to localstorage
+        localStorage.setItem('token', response.data.token)
+        
+        // - redirect user to other pages like '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(()=>{
+        // if request is bad..
+        //- show an error to the user
+        
+      })
+    
+    
   }
-  
-  // if request is good..
-  // - update state to indicated user is authenticated
-  // - save the JWT token
-  // - redirect user to other pages like '/feature'
-  
-  
-  // if request is bad..
-  //- show an error to the user
 }
