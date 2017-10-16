6c6ade798edbae1f7e624460cc8992029e368f54
Song Ji <songji0509@gmail.com>
2017-10-16 09:02:46 -0700

    Add highr order component to control display of feature

--- src/actions/index.js
+++ src/actions/index.js
@@ -31,6 +31,22 @@ export function signinUser({email, password}){
   }
 }
 
+export function signupUser({email, password}){
+  return function(dispatch){
+    axios.post(`${ROOT_URL}/signup`, {email, password})
+    .then(response => {
+      dispatch({type: AUTH_USER})
+      localStorage.setItem('token', response.data.token)
+      browserHistory.push('/feature');
+    })
+    .catch((error)=>{
+      // if request is bad..
+      //- show an error to the user
+      dispatch(authError('Bad Login Info'))
+    })
+  }
+}
+
 export function authError(error){
   return {
     type: AUTH_ERROR,
